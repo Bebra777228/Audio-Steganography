78c1cc03a861b003cdf6a73842309092fdcc88a1
Politrees <143968312+Bebra777228@users.noreply.github.com>
2024-10-04 13:46:46 +0500

    Update txt2spec.py

--- txt2spec.py
+++ txt2spec.py
@@ -48,22 +48,6 @@ def text_to_spectrogram_image(text, base_width=512, height=256, max_font_size=80
     image = np.where(image > 0, 255, image)
     return image
 
-# Function for creating a spectrogram image with an image
-def image_to_spectrogram_image(image_path, base_width=512, height=256, margin=10):
-    image = Image.open(image_path).convert('L')
-    image_width, image_height = image.size
-
-    # Adjust width and height based on image size
-    width = max(base_width, image_width + margin * 2)
-    height = max(height, image_height + margin * 2)
-
-    new_image = Image.new('L', (width, height), 'black')
-    new_image.paste(image, ((width - image_width) // 2, (height - image_height) // 2))
-
-    new_image = np.array(new_image)
-    new_image = np.where(new_image > 0, 255, new_image)
-    return new_image
-
 # Converting an image to audio
 def spectrogram_image_to_audio(image, sr=DEFAULT_SAMPLE_RATE):
     flipped_image = np.flipud(image)
@@ -74,30 +58,6 @@ def spectrogram_image_to_audio(image, sr=DEFAULT_SAMPLE_RATE):
 # Function for creating an audio file and spectrogram from text
 def create_audio_with_spectrogram(text, base_width, height, max_font_size, margin, letter_spacing):
     spec_image = text_to_spectrogram_image(text, base_width, height, max_font_size, margin, letter_spacing)
-    y = spectrogram_image_to_audio(spec_image)
-    
-    with tempfile.NamedTemporaryFile(delete=False, suffix='.wav') as temp_audio:
-        audio_path = temp_audio.name
-        sf.write(audio_path, y, DEFAULT_SAMPLE_RATE)
-    
-    # Create spectrogram from audio
-    S = librosa.feature.melspectrogram(y=y, sr=DEFAULT_SAMPLE_RATE)
-    S_dB = librosa.power_to_db(S, ref=np.max)
-    plt.figure(figsize=(10, 4))
-    librosa.display.specshow(S_dB, sr=DEFAULT_SAMPLE_RATE, x_axis='time', y_axis='mel')
-    plt.axis('off')
-    plt.tight_layout(pad=0)
-    
-    with tempfile.NamedTemporaryFile(delete=False, suffix='.png') as temp_spectrogram:
-        spectrogram_path = temp_spectrogram.name
-        plt.savefig(spectrogram_path, bbox_inches='tight', pad_inches=0, transparent=True)
-    plt.close()
-    
-    return audio_path, spectrogram_path
-
-# Function for creating an audio file and spectrogram from an image
-def create_audio_with_spectrogram_from_image(image_path, base_width, height, margin):
-    spec_image = image_to_spectrogram_image(image_path, base_width, height, margin)
     y = spectrogram_image_to_audio(spec_image)
     
     with tempfile.NamedTemporaryFile(delete=False, suffix='.wav') as temp_audio:
@@ -136,16 +96,26 @@ def display_audio_spectrogram(audio_path):
     plt.close()
     return spectrogram_path
 
+# Converting a downloaded image to an audio spectrogram
+def image_to_spectrogram_audio(image_path, sr=DEFAULT_SAMPLE_RATE):
+    image = Image.open(image_path).convert('L')
+    image = np.array(image)
+    y = spectrogram_image_to_audio(image, sr)
+    
+    with tempfile.NamedTemporaryFile(delete=False, suffix='.wav') as temp_audio:
+        img2audio_path = temp_audio.name
+        sf.write(img2audio_path, y, sr)
+    return img2audio_path
+
 # Gradio interface
 def gradio_interface_fn(text, base_width, height, max_font_size, margin, letter_spacing):
     logging.info(f"Generating audio and spectrogram for text: {text}")
     audio_path, spectrogram_path = create_audio_with_spectrogram(text, base_width, height, max_font_size, margin, letter_spacing)
     return audio_path, spectrogram_path
 
-def gradio_image_to_audio_fn(upload_image, base_width, height, margin):
+def gradio_image_to_audio_fn(upload_image):
     logging.info(f"Converting image to audio: {upload_image}")
-    audio_path, spectrogram_path = create_audio_with_spectrogram_from_image(upload_image, base_width, height, margin)
-    return audio_path, spectrogram_path
+    return image_to_spectrogram_audio(upload_image)
 
 def gradio_decode_fn(upload_audio):
     logging.info(f"Generating spectrogram for audio: {upload_audio}")
@@ -174,16 +144,12 @@ with gr.Blocks(title='Audio Steganography', theme=gr.themes.Soft(primary_hue="gr
         with gr.Group():
             with gr.Row(variant='panel'):
                 upload_image = gr.Image(type="filepath", label="Upload image")
-                base_width = gr.Slider(value=512, label="Image Width", visible=False)
-                height = gr.Slider(value=256, label="Image Height", visible=False)
-                margin = gr.Slider(minimum=0, maximum=50, step=1, value=10, label="Indent")
                 convert_button = gr.Button("Convert to audio")
 
             with gr.Column(variant='panel'):
                 output_audio_from_image = gr.Audio(type="filepath", label="Generated audio")
-                output_spectrogram_from_image = gr.Image(type="filepath", label="Spectrogram")
 
-            convert_button.click(gradio_image_to_audio_fn, inputs=[upload_image, base_width, height, margin], outputs=[output_audio_from_image, output_spectrogram_from_image])
+            convert_button.click(gradio_image_to_audio_fn, inputs=[upload_image], outputs=[output_audio_from_image])
 
     with gr.Tab("Audio Spectrogram"):
         with gr.Group():
