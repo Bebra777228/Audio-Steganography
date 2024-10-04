830e4cd9305e2eac94bef4655266c527239c16c8
Politrees <143968312+Bebra777228@users.noreply.github.com>
2024-10-04 12:25:32 +0500

    Update txt2spec.py

--- txt2spec.py
+++ txt2spec.py
@@ -7,7 +7,7 @@ import gradio as gr
 import soundfile as sf
 import os
 
-# Функция для создания изображения спектрограммы с текстом
+# Function for creating a spectrogram image with text
 def text_to_spectrogram_image(text, base_width=512, height=256, max_font_size=80, margin=10, letter_spacing=5):
     font_path = "/usr/share/fonts/truetype/dejavu/DejaVuSans-Bold.ttf"
     if os.path.exists(font_path):
@@ -30,26 +30,26 @@ def text_to_spectrogram_image(text, base_width=512, height=256, max_font_size=80
 
     image = Image.new('L', (width, height), 'black')
     draw = ImageDraw.Draw(image)
-    
+
     text_x = (width - text_width) // 2
     text_y = (height - (text_bbox[3] - text_bbox[1])) // 2
     for char in text:
         draw.text((text_x, text_y), char, font=font, fill='white')
         char_bbox = draw.textbbox((0, 0), char, font=font)
         text_x += char_bbox[2] - char_bbox[0] + letter_spacing
-    
+
     image = np.array(image)
     image = np.where(image > 0, 255, image)
     return image
 
-# Преобразовываем изображение в аудиосигнал
+# Converting an image to audio
 def spectrogram_image_to_audio(image, sr=22050):
     flipped_image = np.flipud(image)
     S = flipped_image.astype(np.float32) / 255.0 * 100.0
     y = librosa.griffinlim(S)
     return y
 
-# Функция для создания аудиофайла и спектрограммы из текста
+# Function for creating an audio file and spectrogram from text
 def create_audio_with_spectrogram(text, base_width, height, max_font_size, margin, letter_spacing):
     spec_image = text_to_spectrogram_image(text, base_width, height, max_font_size, margin, letter_spacing)
     y = spectrogram_image_to_audio(spec_image)
@@ -59,7 +59,7 @@ def create_audio_with_spectrogram(text, base_width, height, max_font_size, margi
     plt.imsave(image_path, spec_image, cmap='gray')
     return audio_path, image_path
 
-# Функция для отображения спектрограммы аудиофайла
+# Function for displaying the spectrogram of an audio file
 def display_audio_spectrogram(audio_path):
     y, sr = librosa.load(audio_path)
     S = librosa.feature.melspectrogram(y=y, sr=sr)
@@ -74,88 +74,65 @@ def display_audio_spectrogram(audio_path):
     plt.close()
     return spectrogram_path
 
-# Преобразование загруженного изображения в спектрограмму звука
+# Converting a downloaded image to an audio spectrogram
 def image_to_spectrogram_audio(image_path, sr=22050):
     image = Image.open(image_path).convert('L')
     image = np.array(image)
     y = spectrogram_image_to_audio(image, sr)
-    audio_path = 'image_to_audio_output.wav'
-    sf.write(audio_path, y, sr)
-    return audio_path
+    img2audio_path = 'image_to_audio_output.wav'
+    sf.write(img2audio_path, y, sr)
+    return img2audio_path
 
-# Интерфейс Gradio
-with gr.Blocks(title='Аудио-Стеганография', theme=gr.themes.Soft(primary_hue="green", secondary_hue="green", spacing_size="sm", radius_size="lg")) as iface:
-    
-    with gr.Group():
-        with gr.Row(variant='panel'):
-            with gr.Column():
-                gr.HTML("<center><h2><a href='https://t.me/pol1trees'>Telegram Канал</a></h2></center>")
-            with gr.Column():
-                gr.HTML("<center><h2><a href='https://t.me/+GMTP7hZqY0E4OGRi'>Telegram Чат</a></h2></center>")
-            with gr.Column():
-                gr.HTML("<center><h2><a href='https://www.youtube.com/channel/UCHb3fZEVxUisnqLqCrEM8ZA'>YouTube</a></h2></center>")
-            with gr.Column():
-                gr.HTML("<center><h2><a href='https://github.com/Bebra777228/Audio-Steganography'>GitHub</a></h2></center>")
-
-    with gr.Tab("Текст в Спектрограмму"):
+# Gradio interface
+with gr.Blocks(title='Audio Steganography', theme=gr.themes.Soft(primary_hue="green", secondary_hue="green", spacing_size="sm", radius_size="lg")) as txt2spec:
+    with gr.Tab("Text to Spectrogram"):
         with gr.Group():
-            text = gr.Textbox(lines=2, placeholder="Введите свой текст:", label="Текст")
+            text = gr.Textbox(lines=2, placeholder="Enter your text:", label="Text")
             with gr.Row(variant='panel'):
-                base_width = gr.Slider(value=512, label="Ширина изображения", visible=False)
-                height = gr.Slider(value=256, label="Высота изображения", visible=False)
-                max_font_size = gr.Slider(minimum=10, maximum=130, step=5, value=80, label="Размер шрифта")
-                margin = gr.Slider(minimum=0, maximum=50, step=1, value=10, label="Отступ")
-                letter_spacing = gr.Slider(minimum=0, maximum=50, step=1, value=5, label="Расстояние между буквами")
-            generate_button = gr.Button("Сгенерировать")
-        
+                base_width = gr.Slider(value=512, label="Image Width", visible=False)
+                height = gr.Slider(value=256, label="Image Height", visible=False)
+                max_font_size = gr.Slider(minimum=10, maximum=130, step=5, value=80, label="Font size")
+                margin = gr.Slider(minimum=0, maximum=50, step=1, value=10, label="Indent")
+                letter_spacing = gr.Slider(minimum=0, maximum=50, step=1, value=5, label="Letter spacing")
+            generate_button = gr.Button("Generate")
+
         with gr.Column(variant='panel'):
             with gr.Group():
-                output_audio = gr.Audio(type="filepath", label="Сгенерированный звук")
-                output_image = gr.Image(type="filepath", label="Спектрограмма")
+                output_audio = gr.Audio(type="filepath", label="Generated audio")
+                output_image = gr.Image(type="filepath", label="Spectrogram")
 
         def gradio_interface_fn(text, base_width, height, max_font_size, margin, letter_spacing):
+            print("\n", text)
             return create_audio_with_spectrogram(text, base_width, height, max_font_size, margin, letter_spacing)
-        
-        generate_button.click(
-            gradio_interface_fn,
-            inputs=[text, base_width, height, max_font_size, margin, letter_spacing],
-            outputs=[output_audio, output_image]
-        )
 
-    with gr.Tab("Изображение в Спектрограмму"):
+        generate_button.click(gradio_interface_fn, inputs=[text, base_width, height, max_font_size, margin, letter_spacing], outputs=[output_audio, output_image])
+
+    with gr.Tab("Image to Spectrogram"):
         with gr.Group():
             with gr.Row(variant='panel'):
-                upload_image = gr.Image(type="filepath", label="Загрузите изображение")
-                convert_button = gr.Button("Конвертировать в аудио")
-            
+                upload_image = gr.Image(type="filepath", label="Upload image")
+                convert_button = gr.Button("Convert to audio")
+
             with gr.Column(variant='panel'):
-                output_audio_from_image = gr.Audio(type="filepath", label="Сгенерированный звук")
+                output_audio_from_image = gr.Audio(type="filepath", label="Generated audio")
 
             def gradio_image_to_audio_fn(upload_image):
                 return image_to_spectrogram_audio(upload_image)
-            
-            convert_button.click(
-                gradio_image_to_audio_fn,
-                inputs=[upload_image],
-                outputs=[output_audio_from_image]
-            )
 
-    with gr.Tab("Спектрограмма аудио"):
+            convert_button.click(gradio_image_to_audio_fn, inputs=[upload_image], outputs=[output_audio_from_image])
+
+    with gr.Tab("Audio Spectrogram"):
         with gr.Group():
             with gr.Row(variant='panel'):
-                upload_audio = gr.Audio(type="filepath", label="Загрузите аудиофайл", scale=3)
-                decode_button = gr.Button("Показать спектрограмму", scale=2)
-            
+                upload_audio = gr.Audio(type="filepath", label="Upload audio", scale=3)
+                decode_button = gr.Button("Show spectrogram", scale=2)
+
             with gr.Column(variant='panel'):
-                decoded_image = gr.Image(type="filepath", label="Спектрограмма аудио")
+                decoded_image = gr.Image(type="filepath", label="Audio Spectrogram")
 
             def gradio_decode_fn(upload_audio):
                 return display_audio_spectrogram(upload_audio)
-            
-            decode_button.click(
-                gradio_decode_fn,
-                inputs=[upload_audio],
-                outputs=[decoded_image]
-            )
 
-iface.launch(share=True)
+            decode_button.click(gradio_decode_fn, inputs=[upload_audio], outputs=[decoded_image])
+
+txt2spec.launch(share=True)
